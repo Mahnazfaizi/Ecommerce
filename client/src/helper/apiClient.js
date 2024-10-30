0801d66e6b31814c030d744a9b2144547b67ab30
Mahnazfaizi <116143372+Mahnazfaizi@users.noreply.github.com>
2024-10-30 19:39:58 +0430

    Update apiClient.js

--- client/src/helper/apiClient.js
+++ client/src/helper/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // this function set the base url of the backend server for axios
 const apiClient = axios.create({
-    baseURL: "https://ecommerce-3-i58l.onrender.com",
+    baseURL: "https://ecommerce-4-pkcv.onrender.com",
 })
 
 export default apiClient;
